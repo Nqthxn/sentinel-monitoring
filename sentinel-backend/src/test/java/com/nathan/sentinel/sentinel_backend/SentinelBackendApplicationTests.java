88ed34c94c37b986c953eb2881094e342490f50b
Nqthxn <nathanl1605@gmail.com>
2025-07-25 14:22:24 +0100

    Initial commit: Spring Boot project structure

---
+++ sentinel-backend/src/test/java/com/nathan/sentinel/sentinel_backend/SentinelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.sentinel.sentinel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
