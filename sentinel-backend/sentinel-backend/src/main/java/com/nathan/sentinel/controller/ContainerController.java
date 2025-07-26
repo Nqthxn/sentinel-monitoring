9d65ad0020f29bf624c2941e3ed10e0f4c5cdbf2
Nqthxn <nathanl1605@gmail.com>
2025-07-26 19:29:52 +0100

    feat: Successfully connected to the api and display container list in UI table

--- sentinel-backend/sentinel-backend/src/main/java/com/nathan/sentinel/controller/ContainerController.java
+++ sentinel-backend/sentinel-backend/src/main/java/com/nathan/sentinel/controller/ContainerController.java
@@ -2,6 +2,7 @@ package com.nathan.sentinel.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.nathan.sentinel.service.dto.ContainerStatsDto;
 
 @RestController
 @RequestMapping("/api/v1/containers")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ContainerController {
     private final DockerStatsService dockerStatsService;
 
---
+++ sentinel-frontend/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { ContainerListComponent } from './components/container-list/container-list.component';
+export const routes: Routes = [
+    { path: 'containers', component: ContainerListComponent}
+];
