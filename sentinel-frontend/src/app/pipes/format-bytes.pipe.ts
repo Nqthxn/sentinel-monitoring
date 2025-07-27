c1289a6a3ddf066428ea2b00e2108726e254b875
Nqthxn <nathanl1605@gmail.com>
2025-07-27 13:43:33 +0100

    fix(backend): correct CPU calculation for Windows

--- sentinel-frontend/src/app/pipes/format-bytes.pipe.ts
+++ sentinel-frontend/src/app/pipes/format-bytes.pipe.ts
@@ -15,7 +15,7 @@ export class FormatBytesPipe implements PipeTransform {
 
     const i = Math.floor(Math.log(bytes)/Math.log(k));
 
-    return parseFloat((bytes / Math.pow(i, k)).toFixed(dm)) + ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k,i)).toFixed(dm)) + ' ' + sizes[i - 1];
   }
 
 }
