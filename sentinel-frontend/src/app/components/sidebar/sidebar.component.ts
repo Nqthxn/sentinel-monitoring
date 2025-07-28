d4fa1dfee7e04576d0a1fffafc163aefc289f289
Nqthxn <nathanl1605@gmail.com>
2025-07-28 14:34:46 +0100

    feat: imported FontAwesome module and improved sidebar UI with icons

--- sentinel-frontend/src/app/components/sidebar/sidebar.component.ts
+++ sentinel-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,17 @@
 import { NgClass } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import {  faFontAwesome, faHouse, faBoxOpen } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Import the module
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, NgClass],
+  imports: [RouterLink, NgClass, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  faHouse = faHouse;
+  faBoxOpen = faBoxOpen;
   constructor(public router: Router){}
 }
