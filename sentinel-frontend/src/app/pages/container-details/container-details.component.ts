afb5217156f5d0579da022b8c1f87585a1829f13
Nqthxn <nathanl1605@gmail.com>
2025-07-31 15:17:52 +0100

    feat(frontend): display historical container data with charts

--- sentinel-frontend/src/app/pages/container-details/container-details.component.ts
+++ sentinel-frontend/src/app/pages/container-details/container-details.component.ts
@@ -2,15 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { ContainerData, ContainerService, ContainerStatHistory } from '../../services/container.service';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 @Component({
   selector: 'app-container-details',
-  imports: [CommonModule],
+  imports: [CommonModule, NgxChartsModule],
   templateUrl: './container-details.component.html',
   styleUrl: './container-details.component.scss'
 })
 export class ContainerDetailsComponent implements OnInit{
   historicalData: ContainerStatHistory[] | null = null;
   containerId: string | null = null;
+  cpuChartData: any[] = [];
 
   constructor(private route: ActivatedRoute, private containerService: ContainerService){}
 
@@ -21,6 +24,7 @@ export class ContainerDetailsComponent implements OnInit{
         this.containerService.getContainerHistory(this.containerId).subscribe({
           next: (data) => {
             this.historicalData = data;
+            this.formatDataForChart(data);
             console.log('Historical Data Received : ', data);
           },
           error: (err) => {
@@ -29,4 +33,16 @@ export class ContainerDetailsComponent implements OnInit{
         });
       }
   }
+  formatDataForChart(data: ContainerStatHistory[]): void{
+    const cpuSeries = {
+      name: 'CPU Usage',
+      series: data.map(stat => ({
+        name: new Date(stat.timestamp),
+        value: stat.cpuUsagePercent
+      }))
+    };
+    this.cpuChartData = [cpuSeries];
+  }
+
+
 }
